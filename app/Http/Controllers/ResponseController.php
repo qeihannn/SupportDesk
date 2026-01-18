f11ff04808683d41b1e9219aa676ebff37c7d8ee
qeihannn <binggoqeihan@gmail.com>
2026-01-18 22:12:21 +0700

    feat: create ResponseController for handling ticket responses

---
+++ app/Http/Controllers/ResponseController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Ticket;
+use App\Models\Response;
+use Illuminate\Support\Facades\Auth;
+
+class ResponseController extends Controller
+{
+    public function store(Request $request, Ticket $ticket) {
+        //
+    }
+    
+}
