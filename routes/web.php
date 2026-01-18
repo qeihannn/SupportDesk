5a427917a603ac99e3022d18a08949e52023cc18
qeihannn <binggoqeihan@gmail.com>
2026-01-18 22:13:23 +0700

    feat: update routes to include authentication endpoints

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controller\AuthController;
+use App\Http\Controllers\TicketController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('auth/register', [AuthController::class,
+        'showRegisterForm'])->name('register');
+        
+Route::post('auth/postRegister', [AuthController::class,
+    'register'])->name('postRegister');
+
+Route::get('auth/login', [AuthController::class,
+    'showLoginForm'])->name('login');
+
+Route::post('auth/postLogin', [AuthController::class,
+    'login'])->name('postLogin');
+
+Route::middleware(['auth'])->group(function () {
+    Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+    Route::resource('tickets', TicketController::class);
 });
