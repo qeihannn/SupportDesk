4f8fe1e016ce912d64471b65023e623e93faac49
qeihannn <binggoqeihan@gmail.com>
2026-01-19 09:31:00 +0700

    feat: add tiket.php file

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -4,7 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Ticket extends Model
-{
-    //
+class Ticket extends Model {
+    protected $fillable = [
+        'user_id',
+        'title',
+        'description',
+        'photo',
+        'status'
+
+    ];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function responses() {
+        return $this->hasMany(Response::class);
+    }
 }
